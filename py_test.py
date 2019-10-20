34baa2db4f6d06aa3cab7c662f987d7c42909eb7
l82as <l82as@mail.ru>
2019-10-20 22:52:35 +0400

    test py file

--- py_test.py
+++ py_test.py
@@ -1,15 +1,33 @@
 import subprocess as sub
 
-f_in = open("../input.txt","r")
-f_out = open("../output.txt","w")
-f_er = open("../error.txt", "w")
-k= sub.Popen("../testing.exe",stdin=sub.PIPE, stdout=f_out, shell = True)
-#k = call("testing.exe", stdin=f_in, stdout=f_out, shell=True)
+def print_rez(f_in_date):
+    try:
+        k = sub.check_output("python programs_testing_python/prog_1.py", input=f_in_date, encoding='utf-8')
+    # d=k.communicate(b"3")
+    except:
+        k='wrong'
+    return k
+f_in = open("case_test/case_1/input.txt","r")
+f_out = open("case_test/case_1/output.txt","r")
+l_answer=[]
+for i in f_out:
+    l_answer.append(i.replace(" ","\n"))
+print(l_answer)
+l_test=[]
+for i in f_in:
+    s=i.replace(" ","\n")
+    l_test.append(print_rez(s))
+#f_out = open("case_test/case_1/output.txt","r")
 
-#print(k.communicate(input=b'7\n'))
-
-
+print(l_test)
+count = 0
+for i in range(len(l_test)):
+    if l_test[i] == l_answer[i]:
+        count+=1
+    else:
+        print('wrong answer-\n',l_test[i], 'rigth answer-\n', l_answer[i])
+print('count rigth tests',count,'\nwrong tests', len(l_answer) - count)
 f_in.close()
 f_out.close()
-f_er.close()
+
 
