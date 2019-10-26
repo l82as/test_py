dc67f6fc99827878c4b61daa7ba0a467c64875cd
l82as <l82as@mail.ru>
2019-10-26 09:05:59 +0400

    adding case test for qwest #1

--- gen_test_date.py
+++ gen_test_date.py
@@ -3,21 +3,23 @@ import subprocess as sub
 import os
 
 def print_rez(f_in_date, py_file):
-    try:
-        k = sub.check_output(py_file, input=f_in_date, encoding='utf-8')
+    k = sub.check_output(py_file, input=f_in_date, encoding='utf-8')
     # d=k.communicate(b"3")
-    except:
-        k='wrong'
+
     return k
 
-for i in range(1, 11):
+adr_py = "python original_programs/prog_1.py"
+for i in range(1, 5):
     #os.mkdir("case_test/case_"+str(i)+"/")
-    f = open("case_test/case_"+str(i)+"/input.txt", "w")
-    f_o = open("case_test/case_"+str(i)+"/output.txt", "w")
-    for j in range(10):
-        a = random.randint(2, 10)
-        b = random.randint(4,12)
-        f.write(str(a) +" "+str(b) + "\n")
-        f_o.write(str(a * b) + " " + str(a+b)+"\n")
+    f = open("case_test/case_1/input_"+str(i)+".txt", "w")
+    f_o = open("case_test/case_1/output_"+str(i)+".txt", "w")
+
+    a = random.randint(2, 100)
+    b = random.randint(100, 110000)
+    f.write(str(a) + " " + str(b) + "\n")
+    strBin = (str(a) + "\n" + str(b))
+
+    f_o.write(str(print_rez(strBin, adr_py)))
+
     f.close()
     f_o.close()
